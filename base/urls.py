016fb650fd7d2a0d9f2d72230c3a80e9cf23bb97
fufic123 <semenovmark07@gmail.com>
2023-03-01 11:47:30 +0100

    matches full commit

--- base/urls.py
+++ base/urls.py
@@ -2,6 +2,12 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name=""),
-    
-]
+    path('', views.index, name="index"),
+    path('matches/', views.matches, name="matches"),
+    path('create/', views.create, name="create"),
+    path('coliseum/', views.coliseum, name="coliseum"),
+    path('open/', views.open, name="open"),
+    path('matchmaking/', views.matchmaking, name="matchmaking"),
+    path('matchmaking2/', views.matchmaking2, name="matchmaking2"),
+    path('matchmaking3/', views.matchmaking3, name="matchmaking3"),
+    ]
--- base/views.py
+++ base/views.py
@@ -3,3 +3,22 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     return render(request, 'base/index.html')
+
+def matches(request):
+    return render(request, 'base/matches.html')
+
+def create(request):
+    return render(request, 'base/create.html')
+
+def coliseum(request):
+    return render(request, 'base/coliseum.html')
+
+def open(request):
+    return render(request, 'base/open.html')
+
+def matchmaking(request):
+    return render(request, 'base/matchmaking.html')
+def matchmaking2(request):
+    return render(request, 'base/matchmaking2.html')
+def matchmaking3(request):
+    return render(request, 'base/matchmaking3.html')
