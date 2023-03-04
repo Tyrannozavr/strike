0d1c03c4a5744a46bc2032e1168d9a63fd7b7d38
dmiv <dmitriy40647274@gmail.com>
2023-03-05 01:42:38 +0300

    ajax update works

--- static/js/update_list.script.js
+++ static/js/update_list.script.js
@@ -1,5 +1,15 @@
 let list_friends = $(`#list_friends`);
 let search_field = document.querySelector('.input-search')
 search_field.oninput = function () {
-    console.log('changing')
+    $.ajax({
+        url: '/profiles/user_list/',
+        type: 'GET',
+        data: {'username': search_field.value},
+        beforeSend: function() {
+            list_friends.empty();
+        },
+        success: (response) => {
+            list_friends.append(response);
+        }
+    })
 };
